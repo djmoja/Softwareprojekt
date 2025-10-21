d53f06e621cfc0e969db8ef4a79c10ef27ec7ba2
Fklopp <friedemann.klopp@st.ovgu.de>
2025-10-21 19:47:07 +0200

    Testing the pipeline - work in progress

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(join(__dirname, "../../frontend/dist")));
 
-// Bissl Beispiel API
+// Beispiel-API
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from backend!" });
 });
