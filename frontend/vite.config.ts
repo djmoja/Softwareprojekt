42c1a918ff975794de93ce83a04c7487c1ab68bf
Fklopp <friedemann.klopp@st.ovgu.de>
2025-10-21 18:08:46 +0200

    Testing the pipeline - work in progress

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tsconfigPaths({
+      root: './', // or './frontend' if running from root
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
