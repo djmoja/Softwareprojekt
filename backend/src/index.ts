9691783bb79972bfd4ce69274c98309a5357b74e
Fklopp <friedemann.klopp@st.ovgu.de>
2025-10-22 17:18:41 +0200

    Changes for the Pipeline based on Express 5 Version

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,11 +10,14 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(join(__dirname, "../frontend/dist")));
 
-// Beispiel-API
 app.get("/api/hello", (req, res) => {
-    res.json({ message: "Hello from backend!" });
+    res.json({message: "Hello from backend!"});
+});
+
+app.use((req, res) => {
+    res.sendFile(join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`✅ Server running on port ${port}`);
 });
