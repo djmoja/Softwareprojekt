9a662fa0fe6ba8c54b4e5203a1c08abe7faf7277
Fklopp <friedemann.klopp@st.ovgu.de>
2025-10-22 17:05:15 +0200

    Pipeline Frontend Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(join(__dirname, "../../frontend/dist")));
+app.use(express.static(join(__dirname, "../frontend/dist")));
 
 // Beispiel-API
 app.get("/api/hello", (req, res) => {
